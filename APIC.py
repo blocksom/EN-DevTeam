4fbb4cf1e95057dc0aacf31a0eac99b76650dabf
paulburk <36640992+paulburk@users.noreply.github.com>
2018-04-10 09:55:35 -0400

    Add files via upload

--- APIC.py
+++ APIC.py
@@ -1,24 +1,41 @@
-import requests, json
+import requests, json #imports necessary standard packages
 
 requests.packages.urllib3.disable_warnings()
 
-class apic:
+class apic: #this creates the apic class
 
+    # This method generates an API Post call to generate a unique service
+    # ticket, necessary for making any APIC-EM Get requests
     def getTicket(self, ip, username, password):
+
+        # These three variables define the necessary inputs to make an API
+        # Post call
         url = "https://" + ip + "/api/v1/ticket"
         header = {"Content-Type": "application/json"}
         body = {"username": username,"password": password}
-    
+
+        # This sets the 'response' variable to the output of the API call,
+        # formatted in json, leveraging the 'post' method of the requests
+        # standard package
         response = requests.post(url, data=json.dumps(body), headers=header).json()
-    
+
+        # This parses the response into just the relevant value, the ticket
         ticket = response["response"]["serviceTicket"]
     
         return ticket
 
+    # This method generates and API Get call to receive the VLANs operating
+    # on the network
     def getVlans(self, ip, ticket):
+
+        # These two variables define the necessary inputs to make an API
+        # Get call, leveraging the service ticket as authentication
         url = "https://" + ip + "/api/v1/vlan/vlan-names"
         header = {"Content-Type": "application/json", "X-Auth-Token": ticket}
-    
+
+        # This sets the 'response' variable to the output of the API call,
+        # formatted in json, leveraging the 'get' method of the requests
+        # standard package
         response = requests.get(url, headers=header).json()
     
         return response
--- API_Call.py
+++ API_Call.py
@@ -1,14 +1,20 @@
-import requests, json
-from APIC import apic
+import requests, json #imports necessary standard packages
+from APIC import apic #imports 'apic' class from 'APIC.py' for APIC-EM methods
 
 requests.packages.urllib3.disable_warnings()
 
+# Defines global variables
 APIC = 'devnetapi.cisco.com/sandbox/apic_em'
 USERNAME = 'devnetuser'
 PASSWORD = 'Cisco123!'
-    
+
+# This calls on the getTicket method to generate a unique service ticket,
+# necessary for making any API calls to the APIC-EM
 newTicket = apic().getTicket(APIC, USERNAME, PASSWORD)
 
+# This will later live under a lengthy 'if' statement depending on what
+# request the user has made in the body of their Spark message, but presently
+# it's an example of an API call and returns the json response
 msg = apic().getVlans(APIC, newTicket)
 print(msg)
 print(type(msg["response"]))
